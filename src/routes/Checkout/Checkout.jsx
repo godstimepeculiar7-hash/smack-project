cb884f0cbf6f4dd2ad18f021c2f9cff1ee10a65a
Godstime Peculiar <godstimepeculiar7@gmail.com>
2026-05-28 22:17:43 +0100

    Starting the checkout page again, but this time with the backend

--- src/routes/Checkout/Checkout.jsx
+++ src/routes/Checkout/Checkout.jsx
@@ -1,161 +1,26 @@
 import './Checkout.scss';
 import './checkout-header.css';
-import { useContext, useState, useEffect } from 'react';
-import { CartContext } from '../../backend/Cart';
-import { useNavigate } from 'react-router-dom';
-import riceProducts from '../../My Products/Rice';
-import { formatCurrency } from '../../component/Our Best Sellers Desktop/Money/Money';
-import { deliveryOptions } from '../../backend/deliveryOptions';
-import swallow from '../../My Products/Swallow';
-import { Products } from '../../component/Our Best Sellers Desktop/products';
-import dayjs from 'dayjs';
-import { useFlutterwave } from 'flutterwave-react-v3';
-import Swal from 'sweetalert2';
-import { getDistance } from 'geolib';
-import axios from 'axios';
-
 
 function Checkout() {
-  const { cart, totalQuantity, removeFromCart, updateDeliveryOption, totalCost, shippingCost, totalBeforeTax, tax, orderTotal, clearCart } = useContext(CartContext);
-  const navigate = useNavigate();
-  const [currentTime, setCurrentTime] = useState(dayjs());
-
-  const home = () => {
-    navigate('/');
-  }
-
-  const success = () => {
-    Swal.fire({
-      title: 'Payment Successful!',
-      text: 'Your order has been placed successfully.',
-      icon: 'success',
-      confirmButtonText: 'OK',
-      allowOutsideClick: false,
-    })
-    clearCart();
-    navigate('/orders');
-  }
-
-  const failed = () => {
-    Swal.fire({
-      title: 'Payment Cancelled',
-      text: 'There was an issue processing your payment. Please try again.',
-      icon: 'info',
-      confirmButtonText: 'OK',
-      allowOutsideClick: false
-    })
-  }
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentTime(dayjs());
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, []);
-
-  const config = {
-    public_key: 'FLWPUBK_TEST-1d461d516381ef52f84e8bd8440d95a4-X',
-    tx_ref: `${Date.now()}`, // String format is safer
-    amount: orderTotal,
-    currency: 'NGN',
-    payment_options: 'card,mobilemoney,ussd',
-    customer: {
-      email: 'peculiargodstime1@gmail.com',
-      phone_number: '09070850162',
-      name: 'Godstime Peculiar',
-    },
-    customizations: {
-      title: 'Smack Foods',
-      description: 'Total Order',
-    },
-  };
-
-  //Initialize the hook
-  const handleFlutterPayment = useFlutterwave(config);
-
-  const getUserLocation = () => {
-    navigator.geolocation.getCurrentPosition( async (position) => {
-
-      const userLatitude = position.coords.latitude;
-      const userLongitude = position.coords.longitude;
-
-      console.log('userLatitude:', userLatitude);
-      console.log('userLongitude:', userLongitude);
-
-      try{
-        const response = await axios.get(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${userLatitude}&lon=${userLongitude}`)
-        console.log('Full Address:', response.data.display_name);
-      } catch (error) {``
-        console.log('Error fetching address:', error)
-      }
-
-      // Shop location
-      const shopLatitude = 4.8156;
-      const shopLongitude = 7.0498;
-
-      // Calculate distance INSIDE callback
-      const distance = getDistance(
-        {
-          latitude: userLatitude,
-          longitude: userLongitude
-        },
-        {
-          latitude: shopLatitude,
-          longitude: shopLongitude
-        }
-      );
-
-      console.log('Distance to shop (meters):', distance);
-
-      let paymentCompleted = false;
-
-      if (distance <= 50000) {
-        // User is within 10 km, proceed with payment
-        handleFlutterPayment({
-          callback: (response) => {
-            console.log("Payment Response:", response);
-            if (response.status === 'completed' || response.status === 'successful') {
-              paymentCompleted = true;
-              success();
-            } else {
-              failed();
-            }
-          },
-          onClose: () => {
-            console.log("User closed the modal");
-            if (!paymentCompleted) {
-              failed();
-            }
-          },
-        })
-      } else {
-        Swal.fire({
-          title: 'Out of Delivery Range',
-          text: 'Sorry, we do not deliver to your location.',
-          icon: 'error',
-          confirmButtonText: 'OK',
-          allowOutsideClick: false,
-        })
-      }
-
-    }, (error) => {
-      console.error('Error getting location:', error);
-    });
-
-  };
-
   return (
     <div>
       <div className="checkout-header">
         <div className="header-content">
-
-          <div className="checkout-header-middle-section">
-            Checkout (<div className="return-to-home-link"
-              onClick={home}>{totalQuantity} items</div>)
+          <div className="checkout-header-left-section">
+            <a href="index.html">
+              <img className="logo" src="images/logo.png" />
+              <img className="mobile-logo" src="images/mobile-logo.png" />
+            </a>
           </div>
 
+          <div className="checkout-header-middle-section">
+            Checkout (<a className="return-to-home-link"
+              href="index.html">3 items</a>)
+          </div>
 
+          <div className="checkout-header-right-section">
+            <img src="images/icons/checkout-lock-icon.png" />
+          </div>
         </div>
       </div>
 
@@ -164,131 +29,188 @@ function Checkout() {
 
         <div className="checkout-grid">
           <div className="order-summary">
+            <div className="cart-item-container">
+              <div className="delivery-date">
+                Delivery date: Tuesday, June 21
+              </div>
 
-            {cart.map((cartItem) => {
+              <div className="cart-item-details-grid">
+                <img className="product-image"
+                  src="images/products/athletic-cotton-socks-6-pairs.jpg" />
 
-              const allProducts = [...riceProducts, ...swallow, ...Products];
-
-              const matchingProduct = allProducts.find((product) => {
-                return product.id === cartItem.id
-
-              });
-
-              const selectedOption = deliveryOptions.find((option) => {
-                return option.id === cartItem.deliveryOptionId
-              });
-
-              return (
-                <div className="cart-item-container">
-                  <div className="delivery-date">
-                    Delivery Time: {selectedOption.deliveryHours} hour delivery
+                <div className="cart-item-details">
+                  <div className="product-name">
+                    Black and Gray Athletic Cotton Socks - 6 Pairs
                   </div>
+                  <div className="product-price">
+                    $10.90
+                  </div>
+                  <div className="product-quantity">
+                    <span>
+                      Quantity: <span className="quantity-label">2</span>
+                    </span>
+                    <span className="update-quantity-link link-primary">
+                      Update
+                    </span>
+                    <span className="delete-quantity-link link-primary">
+                      Delete
+                    </span>
+                  </div>
+                </div>
 
-                  <div className="cart-item-details-grid">
-                    <img className="product-image"
-                      src={matchingProduct.image} />
-
-                    <div className="cart-item-details">
-                      <div className="product-name">
-                        {matchingProduct.name}
+                <div className="delivery-options">
+                  <div className="delivery-options-title">
+                    Choose a delivery option:
+                  </div>
+                  <div className="delivery-option">
+                    <input type="radio" checked
+                      className="delivery-option-input"
+                      name="delivery-option-1" />
+                    <div>
+                      <div className="delivery-option-date">
+                        Tuesday, June 21
                       </div>
-                      <div className="product-price">
-                        {matchingProduct.priceCents}
-                      </div>
-                      <div className="product-quantity">
-                        <span>
-                          Quantity: <span className="quantity-label">{cartItem.quantity}</span>
-                        </span>
-                        <span className="update-quantity-link link-primary">
-                          Update
-                        </span>
-                        <span className="delete-quantity-link link-primary" onClick={() => {
-                          removeFromCart(cartItem.id)
-                        }}>
-                          Delete
-                        </span>
+                      <div className="delivery-option-price">
+                        FREE Shipping
                       </div>
                     </div>
-
-                    <div className="delivery-options">
-                      <div className="delivery-options-title">
-                        Choose a delivery option:
+                  </div>
+                  <div className="delivery-option">
+                    <input type="radio"
+                      className="delivery-option-input"
+                      name="delivery-option-1" />
+                    <div>
+                      <div className="delivery-option-date">
+                        Wednesday, June 15
                       </div>
-                      {deliveryOptions.map((option) => {
-                        let priceString = 'Free Shipping';
-
-                        if (option.priceCents > 0) {
-                          priceString = `${option.priceCents} - Shipping`
-                        }
-
-                        return (
-                          <div key={option.id} className="delivery-option">
-                            <input type="radio" checked={option.id === cartItem.deliveryOptionId}
-                              className="delivery-option-input"
-                              onChange={() => {
-                                updateDeliveryOption(cartItem.id, option.id)
-                              }}
-                              name={`delivery-option-${cartItem.id}`} />
-                            <div>
-                              <div className="delivery-option-date">
-                                {currentTime.add(option.deliveryHours, 'hour').format('HH:mm A, ')}
-                              </div>
-                              <div className="delivery-option-price">
-                                {priceString}
-                              </div>
-                            </div>
-                          </div>
-                        )
-                      })}
-
-
+                      <div className="delivery-option-price">
+                        $4.99 - Shipping
+                      </div>
+                    </div>
+                  </div>
+                  <div className="delivery-option">
+                    <input type="radio"
+                      className="delivery-option-input"
+                      name="delivery-option-1" />
+                    <div>
+                      <div className="delivery-option-date">
+                        Monday, June 13
+                      </div>
+                      <div className="delivery-option-price">
+                        $9.99 - Shipping
+                      </div>
                     </div>
                   </div>
                 </div>
-              )
-            })}
+              </div>
+            </div>
 
+            <div className="cart-item-container">
+              <div className="delivery-date">
+                Delivery date: Wednesday, June 15
+              </div>
 
+              <div className="cart-item-details-grid">
+                <img className="product-image"
+                  src="images/products/intermediate-composite-basketball.jpg" />
 
+                <div className="cart-item-details">
+                  <div className="product-name">
+                    Intermediate Size Basketball
+                  </div>
+                  <div className="product-price">
+                    $20.95
+                  </div>
+                  <div className="product-quantity">
+                    <span>
+                      Quantity: <span className="quantity-label">1</span>
+                    </span>
+                    <span className="update-quantity-link link-primary">
+                      Update
+                    </span>
+                    <span className="delete-quantity-link link-primary">
+                      Delete
+                    </span>
+                  </div>
+                </div>
+
+                <div className="delivery-options">
+                  <div className="delivery-options-title">
+                    Choose a delivery option:
+                  </div>
+
+                  <div className="delivery-option">
+                    <input type="radio" className="delivery-option-input"
+                      name="delivery-option-2" />
+                    <div>
+                      <div className="delivery-option-date">
+                        Tuesday, June 21
+                      </div>
+                      <div className="delivery-option-price">
+                        FREE Shipping
+                      </div>
+                    </div>
+                  </div>
+                  <div className="delivery-option">
+                    <input type="radio" checked className="delivery-option-input"
+                      name="delivery-option-2" />
+                    <div>
+                      <div className="delivery-option-date">
+                        Wednesday, June 15
+                      </div>
+                      <div className="delivery-option-price">
+                        $4.99 - Shipping
+                      </div>
+                    </div>
+                  </div>
+                  <div className="delivery-option">
+                    <input type="radio" className="delivery-option-input"
+                      name="delivery-option-2" />
+                    <div>
+                      <div className="delivery-option-date">
+                        Monday, June 13
+                      </div>
+                      <div className="delivery-option-price">
+                        $9.99 - Shipping
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
 
           <div className="payment-summary">
-
             <div className="payment-summary-title">
               Payment Summary
             </div>
 
             <div className="payment-summary-row">
-              <div>Items ({totalQuantity}):</div>
-              <div className="payment-summary-money">{totalCost()}</div>
+              <div>Items (3):</div>
+              <div className="payment-summary-money">$42.75</div>
             </div>
 
             <div className="payment-summary-row">
               <div>Shipping &amp; handling:</div>
-              <div className="payment-summary-money">{shippingCost()}</div>
+              <div className="payment-summary-money">$4.99</div>
             </div>
 
             <div className="payment-summary-row subtotal-row">
               <div>Total before tax:</div>
-              <div className="payment-summary-money">{totalBeforeTax}</div>
+              <div className="payment-summary-money">$47.74</div>
             </div>
 
             <div className="payment-summary-row">
               <div>Estimated tax (10%):</div>
-              <div className="payment-summary-money">{tax}</div>
+              <div className="payment-summary-money">$4.77</div>
             </div>
 
             <div className="payment-summary-row total-row">
               <div>Order total:</div>
-              <div className="payment-summary-money">{orderTotal}</div>
+              <div className="payment-summary-money">$52.51</div>
             </div>
 
-            <button className="place-order-button button-primary"
-              onClick={
-                getUserLocation
-
-              }
-            >
+            <button className="place-order-button button-primary">
               Place your order
             </button>
           </div>
