1ee8e1b8ab556e4e48de1641b9adf61fed13b929
Godstime Peculiar <godstimepeculiar7@gmail.com>
2026-05-29 17:19:09 +0100

    Succesfully worked on the radio buttons for the checkout page so the delivery time will change according to the delivery option checked

--- src/routes/Checkout/Checkout.jsx
+++ src/routes/Checkout/Checkout.jsx
@@ -1,26 +1,43 @@
 import './Checkout.scss';
 import './checkout-header.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function Checkout() {
+  const [cartProducts, setCartProducts] = useState([]);
+  const [deliveryOptions, setDeliveryOptions] = useState([]);
+
+  const getCartProducts = async () => {
+    const response = await axios.get('http://localhost:5000/cart');
+    setCartProducts(response.data)
+  };
+
+  useEffect(() => {
+
+    getCartProducts();
+
+    const getDeliveryOptions = async () => {
+      const response = await axios.get('http://localhost:5000/delivery-options');
+      setDeliveryOptions(response.data);
+    }
+
+
+
+    getDeliveryOptions();
+
+
+  }, [])
   return (
     <div>
       <div className="checkout-header">
         <div className="header-content">
-          <div className="checkout-header-left-section">
-            <a href="index.html">
-              <img className="logo" src="images/logo.png" />
-              <img className="mobile-logo" src="images/mobile-logo.png" />
-            </a>
-          </div>
 
           <div className="checkout-header-middle-section">
             Checkout (<a className="return-to-home-link"
-              href="index.html">3 items</a>)
+            >3 items</a>)
           </div>
 
-          <div className="checkout-header-right-section">
-            <img src="images/icons/checkout-lock-icon.png" />
-          </div>
+
         </div>
       </div>
 
@@ -29,155 +46,85 @@ function Checkout() {
 
         <div className="checkout-grid">
           <div className="order-summary">
-            <div className="cart-item-container">
-              <div className="delivery-date">
-                Delivery date: Tuesday, June 21
-              </div>
+            {cartProducts.map((product) => {
 
-              <div className="cart-item-details-grid">
-                <img className="product-image"
-                  src="images/products/athletic-cotton-socks-6-pairs.jpg" />
+              const selectedDeliveryOption = deliveryOptions.find((option) => {
+                return option.id === product.deliveryOptionId;
+              });
 
-                <div className="cart-item-details">
-                  <div className="product-name">
-                    Black and Gray Athletic Cotton Socks - 6 Pairs
+              return (
+                <div className="cart-item-container" key={product.id}>
+                  <div className="delivery-date">
+                    Delivery Time: {selectedDeliveryOption?.deliveryHours} hour delivery
                   </div>
-                  <div className="product-price">
-                    $10.90
-                  </div>
-                  <div className="product-quantity">
-                    <span>
-                      Quantity: <span className="quantity-label">2</span>
-                    </span>
-                    <span className="update-quantity-link link-primary">
-                      Update
-                    </span>
-                    <span className="delete-quantity-link link-primary">
-                      Delete
-                    </span>
-                  </div>
-                </div>
 
-                <div className="delivery-options">
-                  <div className="delivery-options-title">
-                    Choose a delivery option:
-                  </div>
-                  <div className="delivery-option">
-                    <input type="radio" checked
-                      className="delivery-option-input"
-                      name="delivery-option-1" />
-                    <div>
-                      <div className="delivery-option-date">
-                        Tuesday, June 21
+                  <div className="cart-item-details-grid">
+                    <img className="product-image"
+                      src={product.image} />
+
+                    <div className="cart-item-details">
+                      <div className="product-name">
+                        {product.name}
                       </div>
-                      <div className="delivery-option-price">
-                        FREE Shipping
+                      <div className="product-price">
+                        ₦{product.priceCents}
+                      </div>
+                      <div className="product-quantity">
+                        <span>
+                          Quantity: <span className="quantity-label">{product.quantity}</span>
+                        </span>
+                        <span className="update-quantity-link link-primary">
+                          Update
+                        </span>
+                        <span className="delete-quantity-link link-primary">
+                          Delete
+                        </span>
                       </div>
                     </div>
-                  </div>
-                  <div className="delivery-option">
-                    <input type="radio"
-                      className="delivery-option-input"
-                      name="delivery-option-1" />
-                    <div>
-                      <div className="delivery-option-date">
-                        Wednesday, June 15
-                      </div>
-                      <div className="delivery-option-price">
-                        $4.99 - Shipping
-                      </div>
-                    </div>
-                  </div>
-                  <div className="delivery-option">
-                    <input type="radio"
-                      className="delivery-option-input"
-                      name="delivery-option-1" />
-                    <div>
-                      <div className="delivery-option-date">
-                        Monday, June 13
-                      </div>
-                      <div className="delivery-option-price">
-                        $9.99 - Shipping
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
 
-            <div className="cart-item-container">
-              <div className="delivery-date">
-                Delivery date: Wednesday, June 15
-              </div>
+                    <div className="delivery-options">
+                      <div className="delivery-options-title">
+                        Choose a delivery option:
+                      </div>
+                      {deliveryOptions.map((option) => {
+                        return (
+                          <div key={option.id} className="delivery-option">
+                            <input type="radio"
+                              checked={product.deliveryOptionId === option.id}
+                              onChange={async () => {
 
-              <div className="cart-item-details-grid">
-                <img className="product-image"
-                  src="images/products/intermediate-composite-basketball.jpg" />
+                                await axios.put(
+                                  'http://localhost:5000/cart/delivery-option',
+                                  {
+                                    productId: product.id,
+                                    deliveryOptionId: option.id
+                                  }
+                                );
 
-                <div className="cart-item-details">
-                  <div className="product-name">
-                    Intermediate Size Basketball
-                  </div>
-                  <div className="product-price">
-                    $20.95
-                  </div>
-                  <div className="product-quantity">
-                    <span>
-                      Quantity: <span className="quantity-label">1</span>
-                    </span>
-                    <span className="update-quantity-link link-primary">
-                      Update
-                    </span>
-                    <span className="delete-quantity-link link-primary">
-                      Delete
-                    </span>
-                  </div>
-                </div>
+                                await getCartProducts();
 
-                <div className="delivery-options">
-                  <div className="delivery-options-title">
-                    Choose a delivery option:
-                  </div>
+                              }}
+                              className="delivery-option-input"
+                              name={`delivery-option-${product.id}`} />
+                            <div>
+                              <div className="delivery-option-date">
+                                {option.deliveryHours} hour delivery
+                              </div>
+                              <div className="delivery-option-price">
+                                {option.priceCents === 0 ? 'FREE Shipping' : `₦${option.priceCents}`}
+                              </div>
+                            </div>
+                          </div>
+                        )
+                      })}
 
-                  <div className="delivery-option">
-                    <input type="radio" className="delivery-option-input"
-                      name="delivery-option-2" />
-                    <div>
-                      <div className="delivery-option-date">
-                        Tuesday, June 21
-                      </div>
-                      <div className="delivery-option-price">
-                        FREE Shipping
-                      </div>
-                    </div>
-                  </div>
-                  <div className="delivery-option">
-                    <input type="radio" checked className="delivery-option-input"
-                      name="delivery-option-2" />
-                    <div>
-                      <div className="delivery-option-date">
-                        Wednesday, June 15
-                      </div>
-                      <div className="delivery-option-price">
-                        $4.99 - Shipping
-                      </div>
-                    </div>
-                  </div>
-                  <div className="delivery-option">
-                    <input type="radio" className="delivery-option-input"
-                      name="delivery-option-2" />
-                    <div>
-                      <div className="delivery-option-date">
-                        Monday, June 13
-                      </div>
-                      <div className="delivery-option-price">
-                        $9.99 - Shipping
-                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            </div>
+              )
+            })}
+
+
           </div>
 
           <div className="payment-summary">
